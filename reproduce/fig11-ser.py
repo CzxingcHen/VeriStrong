356433d85e492699977066a890f0d7b4da6e8007
rikka <rikka@tsinghua.lab>
2025-06-30 15:08:02 +0800

    tools: dbcop

--- reproduce/fig11-ser.py
+++ reproduce/fig11-ser.py
@@ -11,13 +11,19 @@ history_path = os.path.join(root_path, 'history', 'fig11', 'ser')
 results = []
 
 setups = [
+  # {
+  #   'name': 'veristrong',
+  #   'checker': os.path.join(root_path, 'precompiled', 'builddir-release-veristrong', 'checker'),
+  #   'pruning': 'fast',
+  #   'solver': 'acyclic-minisat', 
+  #   'isolation-level': 'ser',
+  #   'history-type': 'dbcop',
+  # }, 
   {
-    'name': 'veristrong',
-    'checker': os.path.join(root_path, 'precompiled', 'builddir-release-veristrong', 'checker'),
-    'pruning': 'fast',
-    'solver': 'acyclic-minisat', 
-    'isolation-level': 'ser',
-    'history-type': 'dbcop',
+    'name': 'cobra',
+    'checker': os.path.join(root_path, 'tools', 'CobraVerifier'),
+    'config': os.path.join(root_path, 'tools', 'CobraVerifier', 'cobra.conf.nogpu'),
+    'history-type': 'cobra',
   }, 
 ]
 
@@ -30,7 +36,12 @@ def run_single(setup, history):
           '--isolation-level', setup['isolation-level']]  
   elif setup['name'] == 'cobra':
     # TODO
-    pass
+    cobra_path = setup['checker']
+    config_path = setup['config']
+    cmd = ['java',
+           f'-Djava.library.path={cobra_path}/include/:{cobra_path}/build/monosat',
+           '-jar', f"{cobra_path}/target/CobraVerifier-0.0.1-SNAPSHOT-jar-with-dependencies.jar", 
+           'mono', 'audit', config_path, history]  
   elif setup['name'] == 'dbcop':
     # TODO
     pass
@@ -38,8 +49,11 @@ def run_single(setup, history):
     assert False
   
   st_time = time.time()
-  subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+  try:
+    subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, timeout=120)
   # subprocess.run(cmd)
+  except subprocess.TimeoutExpired as e:
+    return 'TO'
   ed_time = time.time()
   duration = ed_time - st_time
   return duration
@@ -47,7 +61,8 @@ def run_single(setup, history):
 def run(setup):
   isolation_path = os.path.join(history_path, setup['history-type'])
   for hist in os.listdir(isolation_path):
-    runtime = run_single(setup, os.path.join(isolation_path, hist, 'history.bincode')) 
+    history = os.path.join(isolation_path, hist, 'history.bincode') if setup['history-type'] == 'dbcop' else os.path.join(isolation_path, hist)
+    runtime = run_single(setup, history) 
     results.append({
       'tool': setup['name'], 
       'hist': hist,
